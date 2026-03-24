25fc4558f4b767a907c0ad46f8edb54f7146afe8
Hee Jin <hijeon80@gmail.com>
2026-03-24 14:04:57 +0900

    second commit

---
+++ 02_counter/main.js
@@ -0,0 +1,31 @@
+let count = 0;
+const num = document.getElementById("result");
+
+function plus(){
+    count++;
+    num.innerHTML = count;
+}
+function plusTwo(){
+    count+=2;
+    num.innerHTML = count;
+}
+function minus(){
+    count--;
+    num.innerHTML = count;
+}
+
+
+// const num = document.querySelector("#plus");
+// const plusButton = document.getElementsByTagName("button")[0];
+
+// plusButton.addEventListener("click", () => {
+//     count++;
+//     num.innerHTML = count;
+// });
+
+// plusButton.onclick=()=>{
+//     count++;
+//     num.innerHTML = count;
+// };
+
+num.innerHTML = count;
