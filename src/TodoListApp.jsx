4170bfffb8d2b4fa0ab31e40b9c2dbc267583ff9
Hee4885 <hijeon80@gmail.com>
2026-04-17 09:10:06 +0900

    TodoListApp addTodo()

--- src/TodoListApp.jsx
+++ src/TodoListApp.jsx
@@ -1,29 +1,42 @@
+import { useState } from 'react';
+
 import './todolist.css'
-import Button from './components/Button.jsx'
+import TOdoItem from './components/TodoItem.jsx'
+import './components/Button'
+import Button from './components/Button';
 import TodoItemEmpty from './components/TodoItemEmpty.jsx'
-import TodoHeader from './components/TodoHeader.jsx'
-import Checkbox from './components/Checkbox.jsx'
+import TodoHeader from './components/TodoHeader.jsx';
+import Checkbox from './components/Checkbox.jsx';
 import TodoAdder from './components/TodoAdder.jsx'
-import TodoItem from './components/TodoItem.jsx'
 import TodoList from './components/TodoList.jsx'
 
-function TodoListApp() {
-    return (
-        <div className='todo'>
-            {/* react 컴포넌트 만들기 */}
-            <TodoHeader />
+class Todo {
+    constructor(text){
+        this.id = Date.now(); // 할일 고유 id 
+        this.text = text; // 할일 내용
+        this.isCompleted = false; //완료 여부
+    }
+}
 
-            <TodoAdder />
-            
+function TodoListApp(){
+    const [todos, setTodos] = useState([]); // 할일 목록 : 기본값 빈 리스트
+    const addTodo = (text) => setTodos((todos) => [
+        ...todos, 
+        // 이전 todos 복사하자
+        // newTodo 만들자
+        // 이전 todos에 추가하자
+        new Todo(Text)
+    ]);
+
+    // const addTodo = (text) => setTodos((todos) => [...todos, new Todo(text)]);
+
+    return(
+        <div className="todo">
+            <TodoHeader />
+            <TodoAdder addTodo={addTodo} />
             <TodoList />
         </div>
     )
 }
 
 export default TodoListApp;
-
-
-//{/* li>p{할 일 없음} */}
-// <li>
-//     <p>할 일 없음</p>
-// </li>
--- src/components/TodoAdder.jsx
+++ src/components/TodoAdder.jsx
@@ -1,9 +1,34 @@
+import { useState } from 'react'
 import Button from './Button.jsx'
 
-export default function TodoAdder() {
+export default function TodoAdder({ addTodo }) {
+    // console.log(addTodo);
+
+    const [inputTodo, setInputTodo] = useState('');  // inputTodo : 사용자가 입력한 값
+
+    const handleSubmit = (event) => { // submit 했을 때, 일어나는 event
+        event.preventDefault(); // submit의 기본동작을 막자
+        
+        // 빈칸이면 바로 return
+        if(!inputTodo) return;
+
+        //inputTodo 에서 사용자가 입력한 text 가져오자 -> inputTodo에 이미 들어 있음
+        //addTodo에 그 text 넣자
+        addTodo(inputTodo.trim()); // trim() : 양쪽 공백 제거
+
+        setInputTodo('');  // input 빈 칸 만들자
+    }
+
     return (
-        <form className='todo__form'>
-            <input type="text" placeholder="할 일을 입력하세요." className='todo__input' />
+        <form className='todo__form' onSubmit={handleSubmit}>
+            {/* 사용자가 입력할 때 setInputTodo() 호출 돼서, inputTodo 값에 설정 */}
+            <input
+                type="text"
+                placeholder="할 일을 입력하세요."
+                className='todo__input'
+                value={inputTodo}
+                onChange={(event) => setInputTodo(event.target.value)}
+            />
             <Button type="submit" className='todo__button todo__button--add'>Add</Button>
         </form>
     )
