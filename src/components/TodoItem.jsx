a718f024a44f3db704714a1d1eb2a22ff5752ee2
Hee4885 <hijeon80@gmail.com>
2026-04-20 13:43:17 +0900

    TodoListApp show todos

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,11 +1,14 @@
 import Button from './Button.jsx'
 import Checkbox from './Checkbox.jsx'
 
-export default function TodoItem() {
+export default function TodoItem({ todo }) {
     return (
-        <li className='todo__item todo__item--comlete'>
+        // <li className='todo__item todo__item--comlete'>
+
+        // todo.isComplted가 true면, " todo__item--comlete" , false면 ""
+        <li className={`todo_item${todo.isComleted?" todo__item--comlete": ""}`}>
             {/* 할 일1 --> children */}
-            <Checkbox id="1">할 일1</Checkbox>
+            <Checkbox id={todo.id}>{todo.text}</Checkbox>
             <Button className='todo__button todo__button--edit'>✏️</Button>
             <Button className='todo__button todo__button--delete'>❌</Button>
         </li>
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,12 +1,22 @@
 import TodoItem from './TodoItem.jsx'
 import TodoItemEmpty from './TodoItemEmpty.jsx'
 
-export default function TodoList() {
+export default function TodoList({ todos }) {
     return (
         <ul className='todo__list'>
+
             {/* react 컴포넌트 만들기 */}
-            <TodoItemEmpty />
-            <TodoItem />
+            {/* todos의 length를 이용해서 empty 인 것을 알 수 있음  -> js 코드라 {}가 있다 */}
+
+
+            {/* todos에 todo 없으면, <TodoItemEmpty /> */}
+            {todos.length === 0 && <TodoItemEmpty />}
+
+            {/* todos에 todo 있으면, <TodoItem /> */}
+            {todos.length > 0 && todos.map((todo) =>
+            <TodoItem key={todo.id} todo={todo} />
+            )}
+
         </ul>
     )
 }
